9b0b19837608a86c8570924c0977b237116491c1
qianxj <qianxj@hzcnc.com>
2016-03-07 11:05:59 +0800

    Update wpsymnode.hpp

--- wpp/wpsymnode.hpp
+++ wpp/wpsymnode.hpp
@@ -9,22 +9,22 @@ namespace xl {namespace wp {
 enum SymKind : char
 {
 	kSymUnknown 	= 0,
-	kSymAxis 			= 1,
-	kSymOneof 		= 2,
-	kSymOption 		= 3,
-	kSymList 			= 4,
-	kSymVar				= 5,
-	kSymAssign			= 6,
-	kSymAction			= 7,
-	kSymKeyword 		= 8,
-	kSymLiteral   		= 9,
-	kSymOperate		= 10,
-	kSymSyn			= 11,
-	kSymSynTerm		= 12,
-	kSymGuard			= 13,
-	kSymSatisfy  		= 14,
-	kSymStmt			= 15,
-	kLiteral				= 16
+	kSymAxis 	= 1,
+	kSymOneof 	= 2,
+	kSymOption 	= 3,
+	kSymList 	= 4,
+	kSymVar		= 5,
+	kSymAssign	= 6,
+	kSymAction	= 7,
+	kSymKeyword 	= 8,
+	kSymLiteral   	= 9,
+	kSymOperate	= 10,
+	kSymSyn		= 11,
+	kSymSynTerm	= 12,
+	kSymGuard	= 13,
+	kSymSatisfy  	= 14,
+	kSymStmt	= 15,
+	kLiteral	= 16
 };
 
 template<class TKind>	
@@ -107,21 +107,21 @@ SymLiteral*  makeLiteralEx(T &val)
 	term->val = new util::misc::TValue(val);
 	return term;
 }
-inline SymLiteral*  makeLiteral(long long int val)						{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(unsigned long long int val) 		{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(int val)									{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(unsigned int val)						{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(short val)								{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(unsigned short val)					{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(char val) 								{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(unsigned char val)					{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(wchar_t val)							{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(double val)								{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(float val)									{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(wchar_t* val)							{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(char* val)								{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(bool val)									{ return makeLiteralEx(val); } 
-inline SymLiteral*  makeLiteral(void* val)								{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(long long int val)		{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(unsigned long long int val) 	{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(int val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(unsigned int val)		{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(short val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(unsigned short val)		{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(char val) 			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(unsigned char val)		{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(wchar_t val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(double val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(float val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(wchar_t* val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(char* val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(bool val)			{ return makeLiteralEx(val); } 
+inline SymLiteral*  makeLiteral(void* val)			{ return makeLiteralEx(val); } 
 
 //list
 inline SymList* makeSymList(SymNode<SymKind> * data)
